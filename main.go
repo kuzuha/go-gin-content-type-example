5c5497b7350c660f204899eb6e7e9ece760df8cc
Kuzuha Shinoda <kuzuha01@hotmail.com>
2024-03-24 07:01:25 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type (
+	UserFromForm struct {
+		Name string `form:"name"`
+	}
+	UserFromJson struct {
+		Name string `json:"name"`
+	}
+)
+
+func router(g *gin.Engine) {
+	g.POST("/json", func(c *gin.Context) {
+		u := &UserFromJson{}
+		err := c.Bind(u)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"name": u.Name,
+		})
+	})
+	g.POST("/form", func(c *gin.Context) {
+		u := &UserFromForm{}
+		err := c.Bind(u)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"name": u.Name,
+		})
+	})
+	g.GET("/query", func(c *gin.Context) {
+		u := &UserFromForm{}
+		err := c.Bind(u)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"name": u.Name,
+		})
+	})
+}
+
+func main() {
+	g := gin.Default()
+	router(g)
+	g.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
---
+++ main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestJSON(t *testing.T) {
+	g := gin.Default()
+	router(g)
+
+	tests := []struct {
+		name        string
+		method      string
+		contentType string
+		url         string
+		body        string
+		expected    interface{}
+	}{
+		{
+			name:        "from json",
+			method:      "POST",
+			contentType: "application/json",
+			url:         "/json",
+			body:        `{"name": "json user"}`,
+			expected:    `{"name":"json user"}`,
+		},
+		{
+			name:        "from form",
+			method:      "POST",
+			contentType: "application/x-www-form-urlencoded",
+			url:         "/form",
+			body:        `name=form%20user`,
+			expected:    `{"name":"form user"}`,
+		},
+		{
+			name:        "from query",
+			method:      "GET",
+			contentType: "",
+			url:         "/query?name=query%20user",
+			body:        ``,
+			expected:    `{"name":"query user"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			req, e := http.NewRequest(tt.method, tt.url, strings.NewReader(tt.body))
+			if assert.NoError(t, e) {
+				req.Header.Set("Content-Type", tt.contentType)
+				g.ServeHTTP(w, req)
+				if assert.Equal(t, 200, w.Code) {
+					b, e := io.ReadAll(w.Body)
+					if assert.NoError(t, e) {
+						assert.Equal(t, tt.expected, string(b))
+					}
+				}
+			}
+		})
+	}
+
+}
